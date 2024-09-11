f253aab1e0a77bdf78443f5ef39d3970c2b3b63f
fperezgonz <fperezgonz@gmail.com>
2024-09-11 23:05:49 +0200

    Made some generalizations and moved tree walking methods to ProjectionsDslUtil.java

--- src/main/java/solutions/sulfura/projectionsdslintellijplugin/utils/ProjectionsDslCompletionContributor.java
+++ src/main/java/solutions/sulfura/projectionsdslintellijplugin/utils/ProjectionsDslCompletionContributor.java
@@ -1,23 +1,18 @@
 package solutions.sulfura.projectionsdslintellijplugin.utils;
 
-import com.google.common.collect.Lists;
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
-import com.intellij.lang.injection.InjectedLanguageManager;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.*;
-import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.util.PsiTypesUtil;
-import com.intellij.psi.util.PsiUtil;
 import com.intellij.util.ProcessingContext;
-import io.vavr.control.Option;
+import org.apache.commons.lang3.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
-import solutions.sulfura.gend.dtos.ListOperation;
-import solutions.sulfura.projectionsdslintellijplugin.psi.ProjectionsDslProjection;
-import solutions.sulfura.projectionsdslintellijplugin.psi.ProjectionsDslPropertyName;
 import solutions.sulfura.projectionsdslintellijplugin.psi.SimpleTypes;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+
+import static solutions.sulfura.projectionsdslintellijplugin.utils.ProjectionsDslUtil.*;
 
 public class ProjectionsDslCompletionContributor extends CompletionContributor {
 
@@ -33,326 +28,59 @@ public class ProjectionsDslCompletionContributor extends CompletionContributor {
         @Override
         protected void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext context, @NotNull CompletionResultSet result) {
 
-            PsiAnnotation psiAnnotation = getContextAnnotation(parameters);
+
+            PsiElement element = parameters.getPosition();
+            PsiElement originalElement = parameters.getOriginalPosition();
+            PsiAnnotation psiAnnotation = getContextAnnotation(element);
 
             if (psiAnnotation == null) {
                 return;
             }
 
-            List<String> projectionPropertyPath = getPropertyPath(parameters);
 
-            String searchTerm = projectionPropertyPath.isEmpty() ?
-                    "" : projectionPropertyPath.remove(projectionPropertyPath.size() - 1).toLowerCase();
+            List<String> projectionPropertyPath = getPathToProperty(originalElement);
+            String inputtedTextForCurrentProperty = getInputtedTextForCurrentProperty(parameters);
+            String searchTerm = ObjectUtils.firstNonNull(inputtedTextForCurrentProperty, "");
+            projectionPropertyPath.add(searchTerm);
+            PsiType psiType = getPsiTypeFromAnnotatedParameterOrMethodReturnType(psiAnnotation);
 
-            PsiType psiType = getPsiType(psiAnnotation);
-
-            //Get class from canonical text and retrieve class fields for completion
-            if (psiType instanceof PsiClassType psiClassType) {
-
-                JavaPsiFacade javaPsiFacade = JavaPsiFacade.getInstance(psiAnnotation.getProject());
-                PsiClass dtoPsiClass = javaPsiFacade.findClass(solutions.sulfura.gend.dtos.Dto.class.getCanonicalName(), GlobalSearchScope.allScope(psiAnnotation.getProject()));
-
-                if (dtoPsiClass == null) {
-                    return;
-                }
-
-                if (PsiTypesUtil.getClassType(dtoPsiClass).isAssignableFrom(psiClassType)) {
-
-                    PsiClass psiProjectionRootClass = PsiUtil.resolveClassInType(psiType);
-
-                    if (psiProjectionRootClass == null) {
-                        return;
-                    }
-
-                    PsiClass collectionPsiClass = Objects.requireNonNull(JavaPsiFacade.getInstance(dtoPsiClass.getProject())
-                            .findClass(Collection.class.getCanonicalName(), psiClassType.getResolveScope()));
-                    PsiType collectionPsiType = PsiTypesUtil.getClassType(collectionPsiClass);
-
-                    PsiClass psiNestedProjectionRootClass = findClassForNestedProperty(psiProjectionRootClass, projectionPropertyPath, collectionPsiType);
-
-                    if (psiNestedProjectionRootClass == null) {
-                        return;
-                    }
-
-                    PsiField[] fields = psiNestedProjectionRootClass.getAllFields();
-
-                    //TODO filter out properties already declared in the current projection
-                    result.addAllElements(
-                            Arrays.stream(fields)
-                                    .map(PsiField::getName)
-                                    .filter(field -> field.toLowerCase().contains(searchTerm))
-                                    .map(LookupElementBuilder::create)
-                                    .toList()
-                    );
-
-                }
-
+            if (!(psiType instanceof PsiClassType)) {
+                return;
             }
+
+            //Find The projected class for the path of the current element
+            PsiClass psiNestedProjectionRootClass = ProjectionsDslUtil.findProjectedClassAtPath(psiAnnotation, projectionPropertyPath);
+
+            if (psiNestedProjectionRootClass == null) {
+                return;
+            }
+
+            //Find the field that corresponds to the current element and return it
+            PsiField[] fields = psiNestedProjectionRootClass.getAllFields();
+
+            //TODO filter out properties already declared in the current projection
+            result.addAllElements(
+                    Arrays.stream(fields)
+                            .map(PsiField::getName)
+                            .filter(field -> field.toLowerCase().contains(searchTerm.toLowerCase()))
+                            .map(LookupElementBuilder::create)
+                            .toList()
+            );
 
         }
 
-        public PsiClass findClassForNestedProperty(@NotNull PsiClass psiProjectionRootClass, List<String> propertyPath, PsiType collectionPsiType) {
-
-            PsiClass projectionClass = psiProjectionRootClass;
-
-            for (String propertyName : propertyPath) {
-
-                for (PsiField field : projectionClass.getAllFields()) {
-
-                    if (field.getName().equals(propertyName)) {
-
-                        projectionClass = null;
-
-                        if (field.getType() instanceof PsiClassType psiClassType) {
-
-                            projectionClass = PsiTypesUtil.getPsiClass(getNestedDtoType(psiClassType, collectionPsiType));
-
-                            if (projectionClass == null) {
-                                return null;
-                            }
-
-                        }
-
-                        //Could not find the path
-                        if (projectionClass == null) {
-                            return null;
-                        }
-
-                    }
-
-                }
-
-            }
-
-            return projectionClass;
-
-        }
-
-        public PsiClassType getNestedDtoType(@NotNull PsiClassType psiClassType, PsiType collectionPsiType) {
-
-            PsiClass psiClass = psiClassType.resolve();
-
-            if (psiClass == null) {
-                return null;
-            }
-
-            String qualifiedName = psiClass.getQualifiedName();
-
-            JavaPsiFacade javaPsiFacade = JavaPsiFacade.getInstance(psiClass.getProject());
-            PsiClass dtoPsiClass = javaPsiFacade.findClass(solutions.sulfura.gend.dtos.Dto.class.getCanonicalName(), GlobalSearchScope.allScope(psiClass.getProject()));
-
-            if (dtoPsiClass == null) {
-                //It is pointless, The project is not using gen-d
-                return null;
-            }
-
-            //Found it!
-            if (PsiTypesUtil.getClassType(dtoPsiClass).isAssignableFrom(psiClassType)) {
-                return psiClassType;
-            }
-
-            //If it is nested within a known generic type, search for the Dto type in the parameterized type
-            if (Objects.equals(qualifiedName, Option.class.getCanonicalName())
-                    || Objects.equals(qualifiedName, Optional.class.getCanonicalName())
-                    || Objects.equals(qualifiedName, ListOperation.class.getCanonicalName())
-                    || collectionPsiType.isAssignableFrom(psiClassType)) {
-
-                PsiType parameter = psiClassType.getParameters()[0];
-
-                if (parameter instanceof PsiClassType parameterClassType) {
-                    return getNestedDtoType(parameterClassType, collectionPsiType);
-                }
-
-            }
-
-            return null;
-
-        }
-
-        public List<String> getPropertyPath(@NotNull CompletionParameters parameters) {
-
-            List<String> path = new ArrayList<>();
+        public String getInputtedTextForCurrentProperty(@NotNull CompletionParameters parameters) {
 
             int cursorOffset = parameters.getOffset();
-
             PsiElement psiElement = parameters.getOriginalPosition();
 
             //If psiElement is null, it is pointless to go further
             if (psiElement == null) {
-                return path;
-            }
-
-            /*The text being written right now for the current property*/
-            String inputText = psiElement.getText().substring(0, cursorOffset - psiElement.getOriginalElement().getTextOffset());
-            path.add(inputText);
-
-            while (psiElement != null) {
-
-                psiElement = findParentProperty(psiElement);
-                if (psiElement != null) {
-                    path.add(psiElement.getText());
-                }
-
-            }
-
-            return Lists.reverse(path);
-
-        }
-
-
-        public PsiElement findParentProperty(@NotNull PsiElement psiElement) {
-
-            //Find parentProjection
-            while (psiElement != null) {
-
-                //Found!
-                if (psiElement instanceof ProjectionsDslProjection) {
-                    break;
-                }
-
-                psiElement = psiElement.getParent();
-
-            }
-
-            //Find the property name for this projection
-            while (psiElement != null) {
-
-                psiElement = psiElement.getPrevSibling();
-
-                if (psiElement instanceof ProjectionsDslPropertyName) {
-                    return psiElement;
-                }
-
-            }
-
-            return null;
-        }
-
-/* This only works when there are parsing errors
-        public PsiElement findParentProperty(@NotNull PsiElement psiElement) {
-
-            //Find the projection start
-            while (psiElement != null) {
-
-                if (psiElement instanceof LeafPsiElement) {
-
-                    IElementType elementType = ((LeafPsiElement) psiElement).getElementType();
-
-                    if (elementType == SimpleTypes.PROJECTION_END) {
-                        psiElement = skipAdjacentProjection(psiElement);
-                        continue;
-                    }
-
-                    //Found!
-                    if (elementType == SimpleTypes.PROJECTION_START) {
-                        psiElement = psiElement.getPrevSibling();
-                        break;
-                    }
-
-                    psiElement = psiElement.getPrevSibling();
-
-                }
-            }
-
-            //Find the property name for this projection
-            while (psiElement != null) {
-
-                if (psiElement instanceof LeafPsiElement) {
-
-                    IElementType elementType = ((LeafPsiElement) psiElement).getElementType();
-
-                    if (elementType == SimpleTypes.FIELD_NAME) {
-                        return psiElement;
-                    }
-
-                    psiElement = psiElement.getPrevSibling();
-
-                }
-            }
-
-            return null;
-
-        }
-
-        public PsiElement skipAdjacentProjection(@NotNull PsiElement projectionEnd) {
-
-            int nestingDepth = 0;
-
-            PsiElement psiElement = projectionEnd;
-
-            while (psiElement != null) {
-
-                if (psiElement instanceof LeafPsiElement) {
-                    IElementType elementType = ((LeafPsiElement) psiElement).getElementType();
-                    if (elementType == SimpleTypes.PROJECTION_END) {
-                        nestingDepth++;
-                    } else if (elementType == SimpleTypes.PROJECTION_START) {
-                        if (nestingDepth == 0) {
-                            return psiElement.getPrevSibling();
-                        } else {
-                            nestingDepth--;
-                        }
-                    }
-                    psiElement = psiElement.getPrevSibling();
-                }
-            }
-
-            return null;
-
-        }*/
-
-        public PsiAnnotation getContextAnnotation(@NotNull CompletionParameters parameters) {
-
-            PsiElement element = parameters.getPosition();
-            PsiElement injectionHost = InjectedLanguageManager.getInstance(element.getProject()).getInjectionHost(element);
-
-            //Without Context, it is not possible to infer completion candidates
-            if (injectionHost == null) {
                 return null;
             }
 
-            while (injectionHost != null && !(injectionHost instanceof PsiAnnotation)) {
-                injectionHost = injectionHost.getParent();
-            }
-
-            if (injectionHost instanceof PsiAnnotation psiAnnotation) {
-                return psiAnnotation;
-            }
-
-            return null;
-
-        }
-
-        public PsiType getPsiType(@NotNull PsiAnnotation psiAnnotation) {
-
-            //TODO Make it work with annotations that specify the dto class
-            // Retrieve the type annotated by this PsiAnnotation
-            PsiElement parent = psiAnnotation.getParent();
-            PsiType psiType = null;
-
-            if (parent instanceof PsiModifierList) {
-                PsiElement grandParent = parent.getParent();
-                if (grandParent instanceof PsiField psiField) {
-                    // The type of the field
-                    psiType = psiField.getType();
-                } else if (grandParent instanceof PsiParameter psiParameter) {
-                    // The return type of the method
-                    psiType = psiParameter.getType();
-                } else if (grandParent instanceof PsiMethod psiMethod) {
-                    // The return type of the method
-                    psiType = psiMethod.getReturnType();
-                } else if (grandParent instanceof PsiClass) {
-                    //Ignore
-                }
-            } else if (parent instanceof PsiTypeElement psiTypeElement) {
-                // Directly on a type element
-                psiType = psiTypeElement.getType();
-            } else {
-                // Handle other cases, such as local variables or specific language elements
-                System.err.println("Unhandled parent type: " + parent.getClass().getName());
-            }
-
-            return psiType;
+            /*The text being written right now for the current property*/
+            return psiElement.getText().substring(0, cursorOffset - psiElement.getOriginalElement().getTextOffset());
 
         }
 
---
+++ src/main/java/solutions/sulfura/projectionsdslintellijplugin/utils/ProjectionsDslUtil.java
@@ -0,0 +1,307 @@
+package solutions.sulfura.projectionsdslintellijplugin.utils;
+
+import com.google.common.collect.Lists;
+import com.intellij.lang.injection.InjectedLanguageManager;
+import com.intellij.psi.*;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.util.PsiTypesUtil;
+import com.intellij.psi.util.PsiUtil;
+import io.vavr.control.Option;
+import org.jetbrains.annotations.NotNull;
+import solutions.sulfura.gend.dtos.ListOperation;
+import solutions.sulfura.projectionsdslintellijplugin.psi.ProjectionsDslProjection;
+import solutions.sulfura.projectionsdslintellijplugin.psi.ProjectionsDslPropertyName;
+import solutions.sulfura.projectionsdslintellijplugin.psi.SimpleTypes;
+
+import java.util.*;
+
+public class ProjectionsDslUtil {
+
+
+    public static PsiAnnotation getContextAnnotation(@NotNull PsiElement element) {
+
+        PsiElement injectionHost = InjectedLanguageManager.getInstance(element.getProject()).getInjectionHost(element);
+
+        //Without Context, it is not possible to infer completion candidates
+        if (injectionHost == null) {
+            return null;
+        }
+
+        while (injectionHost != null && !(injectionHost instanceof PsiAnnotation)) {
+            injectionHost = injectionHost.getParent();
+        }
+
+        if (injectionHost instanceof PsiAnnotation psiAnnotation) {
+            return psiAnnotation;
+        }
+
+        return null;
+
+    }
+
+    public static List<String> getPathToProperty(@NotNull PsiElement element) {
+
+        List<String> path = new ArrayList<>();
+
+        while (element != null) {
+
+            element = findParentPropertyInOriginalElement(element);
+            if (element != null) {
+                path.add(element.getText());
+            }
+
+        }
+
+        return Lists.reverse(path);
+
+    }
+
+
+    public static PsiElement findParentPropertyInOriginalElement(@NotNull PsiElement psiElement) {
+
+        //Find parentProjection
+        while (psiElement != null) {
+
+            //Found!
+            if (psiElement instanceof ProjectionsDslProjection) {
+                break;
+            }
+
+            psiElement = psiElement.getParent();
+
+        }
+
+        //Find the property name for this projection
+        while (psiElement != null) {
+
+            psiElement = psiElement.getPrevSibling();
+
+            if (psiElement instanceof ProjectionsDslPropertyName) {
+                return psiElement;
+            }
+
+        }
+
+        return null;
+    }
+
+
+    //Use this when there is no original element (such as when there are parsing errors)
+    public PsiElement findParentProperty(@NotNull PsiElement psiElement) {
+
+        //Find the projection start
+        while (psiElement != null) {
+
+            if (psiElement instanceof LeafPsiElement) {
+
+                IElementType elementType = ((LeafPsiElement) psiElement).getElementType();
+
+                if (elementType == SimpleTypes.PROJECTION_END) {
+                    psiElement = skipAdjacentProjection(psiElement);
+                    continue;
+                }
+
+                //Found!
+                if (elementType == SimpleTypes.PROJECTION_START) {
+                    psiElement = psiElement.getPrevSibling();
+                    break;
+                }
+
+                psiElement = psiElement.getPrevSibling();
+
+            }
+        }
+
+        //Find the property name for this projection
+        while (psiElement != null) {
+
+            if (psiElement instanceof LeafPsiElement) {
+
+                IElementType elementType = ((LeafPsiElement) psiElement).getElementType();
+
+                if (elementType == SimpleTypes.FIELD_NAME) {
+                    return psiElement;
+                }
+
+                psiElement = psiElement.getPrevSibling();
+
+            }
+        }
+
+        return null;
+
+    }
+
+    public PsiElement skipAdjacentProjection(@NotNull PsiElement projectionEnd) {
+
+        int nestingDepth = 0;
+
+        PsiElement psiElement = projectionEnd;
+
+        while (psiElement != null) {
+
+            if (psiElement instanceof LeafPsiElement) {
+                IElementType elementType = ((LeafPsiElement) psiElement).getElementType();
+                if (elementType == SimpleTypes.PROJECTION_END) {
+                    nestingDepth++;
+                } else if (elementType == SimpleTypes.PROJECTION_START) {
+                    if (nestingDepth == 0) {
+                        return psiElement.getPrevSibling();
+                    } else {
+                        nestingDepth--;
+                    }
+                }
+                psiElement = psiElement.getPrevSibling();
+            }
+        }
+
+        return null;
+
+    }
+
+    public static PsiClass findClassForNestedProperty(@NotNull PsiClass psiProjectionRootClass, List<String> propertyPath, PsiType collectionPsiType) {
+
+        PsiClass projectionClass = psiProjectionRootClass;
+
+        for (String propertyName : propertyPath) {
+
+            for (PsiField field : projectionClass.getAllFields()) {
+
+                if (field.getName().equals(propertyName)) {
+
+                    projectionClass = null;
+
+                    if (field.getType() instanceof PsiClassType psiClassType) {
+
+                        projectionClass = PsiTypesUtil.getPsiClass(getNestedDtoType(psiClassType, collectionPsiType));
+
+                        if (projectionClass == null) {
+                            return null;
+                        }
+
+                    }
+
+                    //Could not find the path
+                    if (projectionClass == null) {
+                        return null;
+                    }
+
+                }
+
+            }
+
+        }
+
+        return projectionClass;
+
+    }
+
+    public static PsiClassType getNestedDtoType(@NotNull PsiClassType psiClassType, PsiType collectionPsiType) {
+
+        PsiClass psiClass = psiClassType.resolve();
+
+        if (psiClass == null) {
+            return null;
+        }
+
+        String qualifiedName = psiClass.getQualifiedName();
+
+        JavaPsiFacade javaPsiFacade = JavaPsiFacade.getInstance(psiClass.getProject());
+        PsiClass dtoPsiClass = javaPsiFacade.findClass(solutions.sulfura.gend.dtos.Dto.class.getCanonicalName(), GlobalSearchScope.allScope(psiClass.getProject()));
+
+        if (dtoPsiClass == null) {
+            //It is pointless, The project is not using gen-d
+            return null;
+        }
+
+        //Found it!
+        if (PsiTypesUtil.getClassType(dtoPsiClass).isAssignableFrom(psiClassType)) {
+            return psiClassType;
+        }
+
+        //If it is nested within a known generic type, search for the Dto type in the parameterized type
+        if (Objects.equals(qualifiedName, Option.class.getCanonicalName())
+                || Objects.equals(qualifiedName, Optional.class.getCanonicalName())
+                || Objects.equals(qualifiedName, ListOperation.class.getCanonicalName())
+                || collectionPsiType.isAssignableFrom(psiClassType)) {
+
+            PsiType parameter = psiClassType.getParameters()[0];
+
+            if (parameter instanceof PsiClassType parameterClassType) {
+                return getNestedDtoType(parameterClassType, collectionPsiType);
+            }
+
+        }
+
+        return null;
+
+    }
+
+
+    public static PsiType getPsiTypeFromAnnotatedParameterOrMethodReturnType(@NotNull PsiAnnotation psiAnnotation) {
+
+        //TODO Make it work with annotations that specify the dto class
+        // Retrieve the type annotated by this PsiAnnotation
+        PsiElement parent = psiAnnotation.getParent();
+        PsiType psiType = null;
+
+        if (parent instanceof PsiModifierList) {
+            PsiElement grandParent = parent.getParent();
+            if (grandParent instanceof PsiField psiField) {
+                // The type of the field
+                psiType = psiField.getType();
+            } else if (grandParent instanceof PsiParameter psiParameter) {
+                // The return type of the method
+                psiType = psiParameter.getType();
+            } else if (grandParent instanceof PsiMethod psiMethod) {
+                // The return type of the method
+                psiType = psiMethod.getReturnType();
+            } else if (grandParent instanceof PsiClass) {
+                //Ignore
+            }
+        } else if (parent instanceof PsiTypeElement psiTypeElement) {
+            // Directly on a type element
+            psiType = psiTypeElement.getType();
+        } else {
+            // Handle other cases, such as local variables or specific language elements
+            System.err.println("Unhandled parent type: " + parent.getClass().getName());
+        }
+
+        return psiType;
+
+    }
+
+    public static PsiClass findProjectedClassAtPath(PsiAnnotation psiAnnotation, List<String> projectionPropertyPath) {
+
+        PsiType psiType = getPsiTypeFromAnnotatedParameterOrMethodReturnType(psiAnnotation);
+
+        if (!(psiType instanceof PsiClassType psiClassType)) {
+            return null;
+        }
+
+        //Find The projected class for the root of the Dsl text
+        JavaPsiFacade javaPsiFacade = JavaPsiFacade.getInstance(psiAnnotation.getProject());
+        PsiClass dtoPsiClass = javaPsiFacade.findClass(solutions.sulfura.gend.dtos.Dto.class.getCanonicalName(), GlobalSearchScope.allScope(psiAnnotation.getProject()));
+
+        if (dtoPsiClass == null || !PsiTypesUtil.getClassType(dtoPsiClass).isAssignableFrom(psiClassType)) {
+            return null;
+        }
+
+        PsiClass psiProjectionRootClass = PsiUtil.resolveClassInType(psiType);
+
+        if (psiProjectionRootClass == null) {
+            return null;
+        }
+
+        //Find The projected class for the current element
+        PsiClass collectionPsiClass = Objects.requireNonNull(JavaPsiFacade.getInstance(dtoPsiClass.getProject())
+                .findClass(Collection.class.getCanonicalName(), psiClassType.getResolveScope()));
+        PsiType collectionPsiType = PsiTypesUtil.getClassType(collectionPsiClass);
+
+        return findClassForNestedProperty(psiProjectionRootClass, projectionPropertyPath, collectionPsiType);
+
+    }
+
+}
